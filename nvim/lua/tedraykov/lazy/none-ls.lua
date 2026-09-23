980aa71b658fb6b3fff337c4475c1fd1f4e65c28
Teodor Raykov <tedraykov@gmail.com>
2026-09-23 12:26:42 +0300

    nvim deprication warning fix

--- nvim/lua/tedraykov/lazy/none-ls.lua
+++ nvim/lua/tedraykov/lazy/none-ls.lua
@@ -46,7 +46,7 @@ return {
 
 		null_ls.setup({
 			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
+				if client:supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						group = augroup,
